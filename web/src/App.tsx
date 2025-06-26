d73e8c9b5e281c54f80e925ab9fb0eed475e0234
wneill33 <136870730+wneill33@users.noreply.github.com>
2025-06-26 00:43:34 +0000

    init shadcn ui & tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,10 @@
-import reactLogo from './assets/react.svg'
-import appLogo from '/tab_logo.svg'
-import PWABadge from './PWABadge.tsx'
-import './App.css'
+import { Button } from './components/ui/button.tsx'
 
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="TradeIQ logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>TradeIQ</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
-    </>
+    <div>
+      <Button>shad cn</Button>
+    </div>
   )
 }
 
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,12 @@
 import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import tailwindcss from '@tailwindcss/vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   server: {
@@ -13,6 +19,7 @@ export default defineConfig({
 
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: false,
@@ -43,4 +50,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
